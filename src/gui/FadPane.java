35b6924c27d2822d02b96c560ea1f553869f28f0
tayyi <tayyipbasara@gmail.com>
2023-03-22 12:22:13 +0100

    FadPane og FadWindow

--- src/gui/FadPane.java
+++ src/gui/FadPane.java
@@ -1,17 +1,87 @@
 package gui;
 
+import application.controller.Controller;
+import application.model.Fad;
+import application.model.Lager;
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 public class FadPane extends GridPane {
+    private ListView<Fad> fadListView;
 
     public FadPane() {
         this.setHgap(10);
         this.setVgap(10);
         this.setPadding(new Insets(20));
+        this.setGridLinesVisible(false);
 
-        this.add(new Label("Test222"), 0, 0);
+
+        this.add(new Label("Fad"), 0, 0);
+        fadListView = new ListView<>();
+        this.add(fadListView, 0, 1, 1, 10);
+        fadListView.setPrefWidth(200);
+        fadListView.setPrefHeight(200);
+        // fadListView.getItems().setAll(Controller.);
+
+        ChangeListener<Fad> fadChangeListener = (fad, fad2, fad3) -> this.fadChanged();
+        fadListView.getSelectionModel().selectedItemProperty().addListener(fadChangeListener);
+
+        ComboBox<Lager> lagerComboBox = new ComboBox<>();
+        //lagerComboBox.setPromptText("Lager");
+        this.add(lagerComboBox,3,1);
+
+        Button btnTilføj = new Button("Tilføj");
+        this.add(btnTilføj,3,2);
+
+        Button btnOpret = new Button("Opret");
+        this.add(btnOpret, 4,1);
+        btnOpret.setOnAction(event -> this.opret());
+
+        Button btnOpdater = new Button("Opdater");
+        this.add(btnOpdater, 4,2);
+        btnOpdater.setOnAction(event -> this.opdater());
+
+        Button btnSlet = new Button("Slet");
+        this.add(btnSlet, 4,3);
+        btnSlet.setOnAction(event -> this.slet());
+
+
+
+    }
+
+    private void opret() {
+        FadWindow fadWindow = new FadWindow("Opret fad");
+        fadWindow.showAndWait();
+
+        // Wait for the modal dialog to close
+
+        //fadListView.getItems().setAll(Controller.());
+        int index = fadListView.getItems().size() - 1;
+        fadListView.getSelectionModel().select(index);
+    }
+
+    private void opdater() {
+    }
+
+    private void slet() {
+    }
+
+    private void fadChanged() {
+            this.updateControls();
+        }
+
+
+
+        public void updateControls() {
+            Fad fad = fadListView.getSelectionModel().getSelectedItem();
+            if (fad != null) {
+
+        }
+
 
     }
 }
--- src/gui/FadWindow.java
+++ src/gui/FadWindow.java
@@ -1,13 +1,15 @@
 package gui;
 
+import application.model.Fad;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class FadWindow extends Stage {
-    public FadWindow() {
+    public FadWindow(String title) {
         GridPane pane = new GridPane();
 
         initContent(pane);
@@ -16,6 +18,9 @@ public class FadWindow extends Stage {
 
         this.setScene(scene);
     }
+
+    private TextField txfType, txfKapacitet, txfOprindelse;
+    private Label lblError;
 
     private void initContent(GridPane pane) {
         //For testing
@@ -26,7 +31,28 @@ public class FadWindow extends Stage {
         pane.setVgap(10);
 
 
-        pane.add(new Label("Test"), 0, 0);
+        pane.add(new Label("Type"), 0, 0);
+        pane.add(new Label("Kapacitet"), 0, 1);
+        pane.add(new Label("Oprindelse"), 0, 2);
+
+        txfType = new TextField();
+        pane.add(txfType,1,0);
+
+        txfKapacitet = new TextField();
+        pane.add(txfKapacitet,1,1);
+
+        txfOprindelse = new TextField();
+        pane.add(txfOprindelse,1,2);
+
+
+
+
+
+
+
+
+
+
     }
 
 }

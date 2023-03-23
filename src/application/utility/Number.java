08dbf32e7541bbdfc98451db7bf5a1e91bcd8ae6
MadsOverggard <nissum_10@hotmail.com>
2023-03-23 09:38:35 +0100

    Edited Utility number checker

--- src/application/utility/Number.java
+++ src/application/utility/Number.java
@@ -1,10 +1,20 @@
 package application.utility;
 
 public class Number {
-    public static int checker(String str) {
+    public static int checkerInt(String str) {
         int number;
         try {
             number = Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            number = -1;
+        }
+        return number;
+    }
+
+    public static double checkerDouble(String str) {
+        double number;
+        try {
+            number = Double.parseDouble(str);
         } catch (NumberFormatException e) {
             number = -1;
         }

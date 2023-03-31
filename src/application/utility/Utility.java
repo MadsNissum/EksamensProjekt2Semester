cd8e3e3564809f107f9bf1934c07097cae4d93c6
MadsOverggard <nissum_10@hotmail.com>
2023-03-31 10:54:13 +0200

    Fixed small things

--- src/application/utility/Utility.java
+++ src/application/utility/Utility.java
@@ -1,9 +1,28 @@
 package application.utility;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+
+import java.security.SecureRandom;
 import java.util.Optional;
+import java.util.Random;
 
 public class Utility {
+
+    static final private String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    static final private Random rng = new SecureRandom();
+
+    public static char randomChar(){
+        return ALPHABET.charAt(rng.nextInt(ALPHABET.length()));
+    }
+
+    public static String randomUUID(int length){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(randomChar());
+        }
+        return sb.toString();
+    }
+
     public static boolean alert(String titel, String message) {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle(titel);
--- src/gui/App.java
+++ src/gui/App.java
@@ -1,15 +1,20 @@
 package gui;
 
+import application.utility.Utility;
 import javafx.application.Application;
 
-import java.util.ArrayList;
-
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.security.SecureRandom;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) {
-        Application.launch(Start.class);
 
+        System.out.println(Utility.randomUUID(16));
+
+        //Application.launch(Start.class);
     }
+
+
 }
